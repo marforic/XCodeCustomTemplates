99a63ba4ba0099cd1d2c2f50640829cea07e0360
Claudio Marforio <marforio@gmail.com>
2010-06-02 19:15:08 +0200

    added possibility to create TableViewCell subclass with Xib

---
+++ File Templates/Cocoa Touch Classes/Objective-C class/UITableViewCell with Xib.pbfiletemplate/class.h
@@ -0,0 +1,18 @@
+//
+//  «FILENAME»
+//
+//  Copyright 
+//      «UNAME» - «DEVELOPEREMAIL»
+//      «ORGANIZATIONNAME» 
+//      «YEAR»
+//  All rights reserved.
+//
+
+@interface «FILEBASENAMEASIDENTIFIER» : UITableViewCell {
+	@private
+	
+}
+
+//@property(nonatomic, retain)
+
+@end
---
+++ File Templates/Cocoa Touch Classes/Objective-C class/UITableViewCell with Xib.pbfiletemplate/class.m
@@ -0,0 +1,38 @@
+//
+//  «FILENAME»
+//
+//  Copyright 
+//      «UNAME» - «DEVELOPEREMAIL»
+//      «ORGANIZATIONNAME» 
+//      «YEAR»
+//  All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+/*
+@interface «FILEBASENAMEASIDENTIFIER» (PrivateMethods)
+
+@end
+*/
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+#pragma mark Memory Management
+
+- (id)initWithCoder:(NSCoder *)coder {
+	if (self = [super initWithCoder:coder]) {
+		
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[super dealloc];
+}
+
+#pragma mark -
+#pragma mark Cell Methods
+
+
+@end
